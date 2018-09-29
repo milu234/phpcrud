cc703848a7f67b3a671f78d3dcbddc302890f56f
milu234 <milanhazra234.mh@gmail.com>
2018-09-29 07:49:34 +0530

    Insert Done

--- index.php
+++ index.php
@@ -1,76 +1,23 @@
-<?php
-include 'db.php';
-if(isset($_POST['bt-insert']))
-{
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $gender = $_POST['gender'];
-    $address = $_POST['address'];
-    $hobby = implode(",",$_POST['hobby']); //In this case the hobby is an array
+<?php  include('server.php'); ?>
 
-}
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Insert Update Delete Data</title>
+    <link rel ="stylesheet" type = "text/css" href = "style.css">
+	<title>CRUD: CReate, Update, Delete PHP MySQL</title>
 </head>
 <body>
-<center>
-<h2>Insert</h2>
-<from method="post">
-<table>
-<tr>
-<td>Name</td><td><input type="text" name = "name" required /></td>
-</tr>
-
-
-<tr>
-<td>Email</td><td><input type="email" name = "email" required /></td>
-</tr>
-
-
-<tr>
-<td>Gender</td><td><input type="radio" name = "gender" value="male" />Male
-<input type="radio" name = "gender" value="female" />Female
-
-</td>
-</tr>
-
-<tr>
-<td>Address</td><td><textarea name = "address" ></textarea></td>
-</tr>
-
-<tr>
-<td>Hobby</td>
-<td>
-<input type = "checkbox" name = "hobby[]" value = "music" />Music
-<input type = "checkbox" name = "hobby[]" value = "movies" />Movies
-<input type = "checkbox" name = "hobby[]" value = "games" />Games
-<input type = "checkbox" name = "hobby[]" value = "Travel" />Travel
-</td>
-</tr>
-<tr>
-<td>Country</td>
-<td>
-<select name = country>
-<option value="India">India</option>
-<option value="Australia">Australia</option>
-<option value="USA">USA</option>
-<option value="Japan">Japan</option>
-</select>
-</td>
-</tr>
-
-</table>
-<input type = "submit" name="bt-insert" value  ="Insert"/>
-</form>
-
-    </center>
+	<form method="post" action="server.php" >
+		<div class="input-group">
+			<label>Name</label>
+			<input type="text" name="name" value="">
+		</div>
+		<div class="input-group">
+			<label>Address</label>
+			<input type="text" name="address" value="">
+		</div>
+		<div class="input-group">
+			<button class="btn" type="submit" name="save" >Save</button>
+		</div>
+	</form>
 </body>
 </html>
